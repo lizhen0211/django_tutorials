218e18d2dac651eb5a0245c4d835f2d6109902a8
lizhen <lizhen@zhumengyuan.com>
2021-06-04 09:21:27 +0800

    add cookie demo

--- cookie_demo/views.py
+++ cookie_demo/views.py
@@ -11,6 +11,7 @@ class SetCookieView(View):
         # request url 请求域名（xxx.lizhen.com）和Response Headers中Set-Cookie中的Domain（lizhen.com）需要匹配
         # 否则 浏览器的response中 set-cookie里可以看到值，但是浏览器的 Application中Cookies中不能写入cookie
         response.set_cookie('hello', 'django', expires=60 * 60 * 24 * 7, secure=False, domain='lizhen.cn')
+        # response.set_cookie('hello2', 'django2', expires=60 * 60 * 24 * 7, secure=False, domain='lizhen.cn')
         return response
 
 
@@ -19,4 +20,5 @@ class DeleteCookieView(View):
     def delete(self, request):
         response = HttpJsonResponse(status=204)
         response.delete_cookie('hello', domain='lizhen.cn')
+        # response.delete_cookie('hello2', domain='lizhen.cn')
         return response
