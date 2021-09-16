e2f03ac735fa8e273eab7a700d12bfa0f293efef
lizhen <lizhen@zhumengyuan.com>
2021-09-16 17:29:19 +0800

    add resis demo

--- redis_demo/urls.py
+++ redis_demo/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url
 
 from redis_demo.views import SetDjangoRedisCacheView, GetDjangoRedisCacheView, SetCacheView, SetCacheParamView, \
-    SetCacheListView, SetCacheSetView
+    SetCacheListView, SetCacheSetView, HashView
 
 urlpatterns = [
     url(r'^redis/set_cache$', SetDjangoRedisCacheView.as_view()),
     url(r'^redis/get_cache$', GetDjangoRedisCacheView.as_view()),
     url(r'^redis/native_set_cache$', SetCacheView.as_view()),
     url(r'^redis/native_set_cache_param$', SetCacheParamView.as_view()),
+    url(r'^redis/native_hash$', HashView.as_view()),
     url(r'^redis/native_set_cache_list$', SetCacheListView.as_view()),
     url(r'^redis/native_set_cache_set$', SetCacheSetView.as_view()),
 
--- redis_demo/views.py
+++ redis_demo/views.py
@@ -140,22 +140,121 @@ class SetCacheParamView(View):
         redis_connection.append("name", "haha")  # 在name对应的值junxi后面追加字符串haha
         print(redis_connection.mget("name"))
 
-        # 清空当前数据库缓存
-        redis_connection.flushdb()
-        # 删除所有数据库中的所有key
+        # 清空当前库中的所有 key
+        # redis_connection.flushdb()
+        # 清空整个 Redis 服务器的数据
         # redis_connection.flushall()
+        return HttpJsonResponse(status=200)
+
+
+class HashView(View):
+    def post(self, request):
+        # redis 基本命令 hash
+        # 1、单个增加--修改(单个取出)--没有就新增，有的话就修改
+        # hset(name, key, value)
+        # 注：hsetnx(name, key, value) 当name对应的hash中不存在当前key时则创建（相当于添加）
+        redis_connection.hset("hash1", "k1", "v1")
+        redis_connection.hset("hash1", "k2", "v2")
+        print(redis_connection.hkeys("hash1"))  # 取hash中所有的key
+        print(redis_connection.hget("hash1", "k1"))  # 单个
+        print(redis_connection.hmget("hash1", "k1", "k2"))  # 多个取hash的key对应的值# 取hash的key对应的值
+        print(redis_connection.hget("hash1", "k2"))
+
+        # 2、批量增加（取出）
+        # hmset(name, mapping)
+        # 在name对应的hash中批量设置键值对
+        # 参数：
+        #   name - redis的name
+        #   mapping - 字典，如：{'k1':'v1', 'k2': 'v2'}
+        redis_connection.hmset("hash2", {"k2": "v2", "k3": "v3"})
+        print(redis_connection.hget("hash2", "k2"))  # 单个取出"hash2"的key-k2对应的value
+        print(redis_connection.hmget("hash2", "k2", "k3"))  # 批量取出"hash2"的key-k2 k3对应的value --方式1
+        print(redis_connection.hmget("hash2", ["k2", "k3"]))  # 批量取出"hash2"的key-k2 k3对应的value --方式2
+
+        # 3、取出所有的键值对
+        print(redis_connection.hgetall("hash1"))
+
+        # 4、得到所有键值对的格式 hash长度
+        print(redis_connection.hlen("hash1"))
+
+        # 5、得到所有的keys（类似字典的取所有keys）
+        # 获取name对应的hash中所有的key的值
+        print(redis_connection.hkeys("hash1"))
+
+        # 6、得到所有的value（类似字典的取所有value）
+        # 获取name对应的hash中所有的value的值
+        print(redis_connection.hvals("hash1"))
+
+        # 7、判断成员是否存在（类似字典的in）
+        # hexists(name, key)
+        print(redis_connection.hexists("hash1", "k4"))  # False 不存在
+        print(redis_connection.hexists("hash1", "k1"))  # True 存在
+
+        # 8、删除键值对
+        # hdel(name,*keys)
+        # 将name对应的hash中指定key的键值对删除
+        print(redis_connection.hgetall("hash1"))
+        redis_connection.hset("hash1", "k2", "v222")  # 修改已有的key k2
+        redis_connection.hset("hash1", "k11", "v1")  # 新增键值对 k11
+        redis_connection.hdel("hash1", "k1")  # 删除一个键值对
+        print(redis_connection.hgetall("hash1"))
+
+        # 9、自增自减整数(将key对应的value--整数 自增1或者2，或者别的整数 负数就是自减)
+        # hincrby(name, key, amount=1)
+        redis_connection.hset("hash1", "k3", 123)
+        redis_connection.hincrby("hash1", "k3", amount=-1)
+        print(redis_connection.hgetall("hash1"))
+        redis_connection.hincrby("hash1", "k4", amount=1)  # 不存在的话，value默认就是1
+        print(redis_connection.hgetall("hash1"))
+
+        # 10、自增自减浮点数(将key对应的value--浮点数 自增1.0或者2.0，或者别的浮点数 负数就是自减)
+        # hincrbyfloat(name, key, amount=1.0)
+
         return HttpJsonResponse(status=200)
 
 
 class SetCacheListView(View):
     def post(self, request):
+        # redis基本命令 list
+        # 1.增加（类似于list的append，只是这里是从左边新增加）--没有就新建
+        # lpush(name,values)
         redis_connection.lpush("list1", 11, 22, 33)
         print(redis_connection.lrange('list1', 0, -1))
+        # 保存顺序为: 33, 22, 11
 
         redis_connection.rpush("list2", 11, 22, 33)  # 表示从右向左操作
         print(redis_connection.llen("list2"))  # 列表长度
         print(redis_connection.lrange("list2", 0, 3))  # 切片取出值，范围是索引号0-3
 
+        # 2.增加（从右边增加）--没有就新建
+        redis_connection.rpush("list2", 44, 55, 66)  # 在列表的右边，依次添加44,55,66
+        print(redis_connection.llen("list2"))  # 列表长度
+        print(redis_connection.lrange("list2", 0, -1))  # 切片取出值，范围是索引号0到-1(最后一个元素)
+
+        # 3.往已经有的name的列表的左边添加元素，没有的话无法创建
+        # 在name对应的list中添加元素，只有name已经存在时，值添加到列表的最左边
+        redis_connection.lpushx("list10", 10)  # 这里list10不存在
+        print(redis_connection.llen("list10"))  # 0
+        print(redis_connection.lrange("list10", 0, -1))  # []
+        redis_connection.lpushx("list2", 77)  # 这里"list2"之前已经存在，往列表最左边添加一个元素，一次只能添加一个
+        print(redis_connection.llen("list2"))  # 列表长度
+        print(redis_connection.lrange("list2", 0, -1))  # 切片取出值，范围是索引号0到-1(最后一个元素
+
+        # 4.往已经有的name的列表的右边添加元素，没有的话无法创建
+        redis_connection.rpushx("list2", 99)  # 这里"foo_list1"之前已经存在，往列表最右边添加一个元素，一次只能添加一个
+        print(redis_connection.llen("list2"))  # 列表长度
+        print(redis_connection.lrange("list2", 0, -1))  # 切片取出值，范围是索引号0到-1(最后一个元素)
+
+        # 5.新增（固定索引号位置插入元素）
+        # 6.修改（指定索引号进行修改）
+        # 7.删除（指定值进行删除）
+        # 8.删除并返回
+        # 9.删除索引之外的值
+        # 10.取值（根据索引号取值）
+        # 11.移动 元素从一个列表移动到另外一个列表
+        # 12.移动 元素从一个列表移动到另外一个列表 可以设置超时
+        # 13.一次移除多个列表
+        # 14.自定义增量迭代
         return HttpJsonResponse(status=200)
 
 
